2c78e296e815779c4d2e550e42346965d2ac6a03
vanorium <vanorium.dev@gmail.com>
2024-09-28 12:33:34 +0200

    socket defining

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,4 @@
 // const socket = io('localhost:8080')
-console.log(process.env)
-console.log(process.env.NEXT_PUBLIC_SOCKET_URL)
-const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL)
+const socket = io('https://quiz-gvhm.onrender.com')
 
 export default socket
